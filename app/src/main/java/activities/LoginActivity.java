1aa3834d1c3c0b86e1910003cd2305fe2f800300
danielyamamoto <danylarayamamoto@hotmail.com>
2021-07-06 16:51:15 -0500

    Binding view added

--- app/src/main/java/activities/LoginActivity.java
+++ app/src/main/java/activities/LoginActivity.java
@@ -3,12 +3,16 @@ package activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.instragram.R;
+import com.example.instragram.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
 
     private EditText etUsername, etPassword;
     private Button btnLogin;
@@ -16,6 +20,27 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        // Binding library
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        // Layout of activity is stored in a special property called root
+        View view = binding.getRoot();
+        setContentView(view);
+
+        etUsername = binding.etUsername;
+        etPassword = binding.etPassword;
+
+        btnLogin = binding.btnLogin;
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user" + username);
     }
 }
