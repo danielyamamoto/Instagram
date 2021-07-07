16ba3e300abf2722290adb3b8f50014b9899bb63
danielyamamoto <danylarayamamoto@hotmail.com>
2021-07-06 22:37:02 -0500

    Hiding keys with apikey properties

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -5,14 +5,19 @@ import android.app.Application;
 import com.parse.Parse;
 
 public class ParseApplication extends Application {
+
+    public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;       // API KEY
+    public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET; // SECRET KEY
+    public static final String URL_DATABASE = BuildConfig.URL_DATABASE;            // URL DATABASE
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId(getString(R.string.back4app_app_id))
-                .clientKey(getString(R.string.back4app_client_key))
-                .server(getString(R.string.back4app_server_url))
+                .applicationId(REST_CONSUMER_KEY)
+                .clientKey(REST_CONSUMER_SECRET)
+                .server(URL_DATABASE)
                 .build());
     }
 }
