e2e5b91355eb28b6b1f4fd3c6c30f898aae756bc
danielyamamoto <danylarayamamoto@hotmail.com>
2021-07-07 16:15:41 -0500

    Data model & save post in db

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -2,7 +2,9 @@ package com.example.instagram;
 
 import android.app.Application;
 
+import com.example.instagram.utils.Post;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
@@ -13,6 +15,10 @@ public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(REST_CONSUMER_KEY)
--- app/src/main/java/com/example/instagram/activities/MainActivity.java
+++ app/src/main/java/com/example/instagram/activities/MainActivity.java
@@ -3,11 +3,30 @@ package com.example.instagram.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.instagram.databinding.ActivityMainBinding;
+import com.example.instagram.utils.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private EditText etDescription;
+    private ImageView ivPostImage;
+    private Button btnSubmit, btnCaptureImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +36,61 @@ public class MainActivity extends AppCompatActivity {
         // Layout of activity is stored in a special property called root
         View view = binding.getRoot();
         setContentView(view);
+
+        etDescription = binding.etDescription;
+        ivPostImage = binding.ivPostImage;
+        btnCaptureImage = binding.btnCaptureImage;
+        btnSubmit = binding.btnSubmit;
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString();
+                if (description.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Description cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(description, currentUser);
+            }
+        });
+
+       //queryPost();
+    }
+
+    private void savePost(String description, ParseUser currentUser) {
+        Post post = new Post();
+        post.setDescription(description);
+
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(MainActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
+                }
+                Log.i(TAG, "Post save was successful!");
+                etDescription.setText("");
+            }
+        });
+    }
+
+    private void queryPost() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post" + post.getDescription());
+                }
+            }
+        });
     }
 }
